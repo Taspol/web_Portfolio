7f949c73d05e308790909c471f95cd6131bf3a3b
Taspol <taspolsd@gmail.com>
2024-12-25 22:21:40 +0700

    add contact

--- src/component/Contact.tsx
+++ src/component/Contact.tsx
@@ -1,23 +1,43 @@
+'use client'
 import Image from "next/image";
+import { useState } from "react";
 
 export default function Contact() {
+    const [copied, setCopied] = useState("");
+    const [type, setType] = useState("");
+
+    const handleCopy = (text:string,contactType:string) => {
+        navigator.clipboard.writeText(text).then(() => {
+        setCopied(text); 
+        setType(contactType); 
+        setTimeout(() => setCopied(""), 2000); 
+        });
+    };
+
     return (
-        <div className="m-5 p-[2vw] h-100 w-[32vw] bg-white text-center rounded-3xl flex flex-col items-center justify-center">
-                <div className="flex text-center items-center">
-                    <div className="mt-[2vw] text-4xl font-bold mr-5">Contact</div><div className="mt-[2vw] text-6xl font-bold">🙋‍♂️</div>
-                </div>  
-                <div className="mt-[3vw] text-xl leading-loose flex">
-                    <Image 
-                        src="/img/github.png" 
-                        alt="Greeting Image" 
-                        width={60} 
-                        height={50} 
-                        className="rounded-xl mx-2"
-                    />
-                    <div className="mt-3 mx-2">
-                        <div className="">https://github.com/Taspol</div>
-                    </div>
+        <div className="m-5 p-[4vw] h-100 w-[32vw] bg-white text-center rounded-3xl flex flex-col items-center justify-center">
+            <div className="flex text-center items-center">
+                <div className="text-4xl font-bold mr-5">Contact</div>
+            </div>  
+            <div className="flex mt-[2vw]">
+                <div onClick={() => handleCopy("https://github.com/Taspol","Github")} className="cursor-pointer">
+                    <Image src="/img/github.png" alt="GitHub" width={60} height={60} className="m-3"/>
                 </div>
+
+                <div onClick={() => handleCopy("https://www.linkedin.com/in/taspol-sawasdee-a77017264","Linkedin")} className="cursor-pointer">
+                    <Image src="/img/linkedin.png" alt="LinkedIn" width={60} height={60} className="m-3"/>
+                </div>
+
+                <div onClick={() => handleCopy("taspolsd","Line")} className="cursor-pointer">
+                    <Image src="/img/line.png" alt="LINE" width={60} height={60} className="m-3"/>
+                </div>
+
+                <div onClick={() => handleCopy("taspolsd@gmail.com","Gmail")} className="cursor-pointer">
+                    <Image src="/img/gmail.png" alt="Gmail" width={70} height={60} className="m-3"/>
+                </div>
+            </div>
+            {copied && <div className="text-green-500 font-bold mt-2">Copied: {type}</div>}
+            {!copied && <div className="text-gray-500 font-bold mt-2">Click the icon to copy contact</div>}
         </div>
-    );
+  );
 }
